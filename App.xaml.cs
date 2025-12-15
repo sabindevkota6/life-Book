478b672ca903f772d42788fe954bbea820ee9966
Sabin Devkota <sabindevkota6@gmail.com>
2025-12-15 13:26:16 +0545

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,15 @@
+﻿namespace life_Book
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new MainPage()) { Title = "life-Book" };
+        }
+    }
+}
